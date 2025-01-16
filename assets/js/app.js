eeabc4da9e5cd37ea4b2ff02525581c34b7d6df5
URIAN VIERA <urian1213viera@gmail.com>
2025-01-16 15:08:37 -0500

    iniciando desarrollo

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("donas-container");
+
+  // Función para crear una tarjeta HTML
+  function createDonaCard({ name, price, image, category }) {
+    return `
+      <div class="col-md-3">
+        <div class="card h-100 border-0 custom-card">
+          <img src="assets/img-products/${image}.jpg" class="card-img-top" alt="${name}">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">Categoría: <strong>${category}</strong></p>
+            <p class="card-text">Precio: <strong>$${price.toFixed(
+              2
+            )}</strong></p>
+            
+            <button class="btn btn-cart w-100 mt-auto">
+                Agregar al carrito &nbsp; <i class="bi bi-cart-plus"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  // Función para cargar los datos
+  async function loadDonas() {
+    try {
+      const response = await axios.get("./assets/js/data_donas.json");
+      const donas = response.data;
+
+      // Renderizar las tarjetas
+      const donaCards = donas.map(createDonaCard).join("");
+      container.innerHTML = donaCards;
+    } catch (error) {
+      console.error("Error al cargar los datos:", error);
+      container.innerHTML = `<p class="text-danger text-center">Hubo un error al cargar los datos.</p>`;
+    }
+  }
+
+  loadDonas();
+});
